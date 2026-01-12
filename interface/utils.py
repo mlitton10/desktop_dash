49b08ac70cdf3a71949ee26d7d3ad7258eaa7837
mlitton10 <mattlitton10@gmail.com>
2026-01-12 00:28:13 -0800

    Add weather api requests, locality management, timestamp management

--- interface/utils.py
+++ interface/utils.py
@@ -1,4 +1,24 @@
 import tkinter as tk
+from geopy.geocoders import Nominatim
+from datetime import datetime
+from pytz import timezone
+import pytz
+
+address_dict = {"My Home": "807 N Hudson Avenue, Los Angeles, California, United States",
+                "Cait's Home": "11 S Termino Avenue, Long Beach California United States",
+                "Work": "1000 Veteran Ave, Los Angeles, CA 90024"}
+
+def get_lat_long(addresses):
+    geolocator = Nominatim(user_agent='your_app_name')
+    coordinates = {}
+
+    for loc, address in addresses.items():
+        location = geolocator.geocode(address)
+
+        lat, lon = location.latitude, location.longitude
+
+        coordinates[loc] = (lat, lon)
+    return coordinates
 
 
 def get_screen_size():
@@ -12,3 +32,16 @@ def get_screen_size():
 
     root.destroy()  # Close the small Tkinter window that was created
     return screen_width, screen_height
+
+def handle_timestamp(timestamp):
+    LosAngeles = timezone('America/Los_Angeles')
+
+    utc_aware = datetime.fromtimestamp(timestamp, tz=pytz.UTC)
+    local_timezone = LosAngeles.normalize(utc_aware)
+
+    return local_timezone.strftime('%Y-%m-%d %H:%M:%S')
+
+
+if __name__ == "__main__":
+    coords = get_lat_long(address_dict)
+    print(coords)
--- web_service/base_api_handler.py
+++ web_service/base_api_handler.py
@@ -1,2 +1,38 @@
 import requests
 import json
+from interface.utils import get_lat_long
+
+
+class BaseRequest:
+    def __init__(self, url):
+        response = requests.get(url)
+        print(f"Status Code: {response.status_code}")
+
+        self.data = response.json()
+        pass
+
+
+class WeatherRequest(BaseRequest):
+    def __init__(self, coords, n_days=7):
+        api_key = "e5d294f92b8b38cad7c5252af8b68a20"
+
+        api_string = "http://api.openweathermap.org/data/2.5/forecast?lat={}&lon={}&cnt={}&appid={}".format(coords[0], coords[1], n_days, api_key)
+
+        super().__init__(api_string)
+
+class WeatherRequestManager():
+    def __init__(self):
+        address_dict = {"My Home": "807 N Hudson Avenue, Los Angeles, California, United States",
+                        "Cait's Home": "11 S Termino Avenue, Long Beach California United States",
+                        "Work": "1000 Veteran Ave, Los Angeles, CA 90024"}
+
+        coords = get_lat_long(address_dict)
+        self.data_dict = {}
+        for location, coord in coords.items():
+            weather = WeatherRequest(coord)
+            self.data_dict[location] = weather.data
+
+
+if __name__=="__main__":
+    w = WeatherRequestManager()
+    print(w.data_dict)
