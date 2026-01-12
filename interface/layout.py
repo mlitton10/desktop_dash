7d5f7a2dc909079721177cebf8561d69b7eeb6f9
mlitton10 <mattlitton10@gmail.com>
2026-01-11 22:01:15 -0800

    Modify application sizing

--- interface/layout.py
+++ interface/layout.py
@@ -12,11 +12,12 @@ class Dashboard:
 
         screen_width, screen_height = get_screen_size()
 
-        root.geometry("{}x{}".format(screen_width, screen_height))
+        root.geometry("{}x{}".format(int(screen_width*0.9), int(screen_height*0.9)))
 
         w = tk.Label(root, text='Hello World')
         w.pack()
-
+        button = tk.Button(root, text='Stop', width=25, command=root.destroy)
+        button.pack()
         root.mainloop()
 
         pass
