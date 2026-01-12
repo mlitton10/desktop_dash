c1bfaac523b03231c67949feedf0ecfbe885d4e7
mlitton10 <mattlitton10@gmail.com>
2026-01-11 17:57:52 -0800

    Make dashboard class

--- interface/layout.py
+++ interface/layout.py
@@ -3,14 +3,24 @@ import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
 from utils import get_screen_size
 
-root = ttk.Window(themename="darkly")
-root.title("DesktopDash")
 
-screen_width, screen_height = get_screen_size()
 
-root.geometry("{}x{}".format(screen_width, screen_height))
+class Dashboard:
+    def __init__(self):
+        root = ttk.Window(themename="darkly")
+        root.title("DesktopDash")
 
-w = tk.Label(root, text='Hello World')
-w.pack()
+        screen_width, screen_height = get_screen_size()
 
-root.mainloop()
+        root.geometry("{}x{}".format(screen_width, screen_height))
+
+        w = tk.Label(root, text='Hello World')
+        w.pack()
+
+        root.mainloop()
+
+        pass
+
+
+if __name__=="__main__":
+    desktop_dash = Dashboard()
