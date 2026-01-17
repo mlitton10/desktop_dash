1bfb10617f6c90a31d9f4364184273dc08f186e2
mlitton10 <mattlitton10@gmail.com>
2026-01-16 17:41:07 -0800

    Adjustments to text layout

--- interface/widget.py
+++ interface/widget.py
@@ -7,14 +7,15 @@ class WeatherDisplayFrame(ttk.Frame):
         # This is a template frame for a weather display. Fake data for now
         super().__init__(master)
 
-        title = ttk.Label(self, text=list(weather_dict.keys())[0], font=('Arial',50))
-        title.grid(row=0, column=0, padx=10, pady=10, sticky='ew')
+        title = ttk.Label(self, text=list(weather_dict.keys())[0], font=('Arial', 50), justify='center')
+        title.grid(row=0, column=0, padx=40, pady=10, sticky='ew')
 
         for i, (name, value) in enumerate(weather_dict[list(weather_dict.keys())[0]].items()):
-            name_label = ttk.Label(self, text=name)
-            name_label.grid(row=i+1, column=0, padx=10, pady=10, sticky='ew')
+            name_label = ttk.Label(self, text=name, justify='center')
+            name_label.grid(row=i + 1, column=0, padx=40, pady=10, sticky='ew')
 
-            value_label = ttk.Label(self, text=value)
-            value_label.grid(row=i+1, column=1, padx=10, pady=10, sticky='ew')
+            value_label = ttk.Label(self, text=value, justify='center')
+            value_label.grid(row=i + 1, column=1, padx=40, pady=10, sticky='ew')
         pass
+
     pass
