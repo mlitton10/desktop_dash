a58b6ac86c08826d52fe9af3b1678906e8b4f80a
mlitton10 <mattlitton10@gmail.com>
2026-01-16 17:49:17 -0800

    Bigger text better text

--- interface/widget.py
+++ interface/widget.py
@@ -11,10 +11,10 @@ class WeatherDisplayFrame(ttk.Frame):
         title.grid(row=0, column=0, padx=40, pady=10, sticky='ew', columnspan=2)
 
         for i, (name, value) in enumerate(weather_dict[list(weather_dict.keys())[0]].items()):
-            name_label = ttk.Label(self, text=name, justify='center')
+            name_label = ttk.Label(self, text=name,font=('Arial', 20), justify='center')
             name_label.grid(row=i + 1, column=0, padx=40, pady=10, sticky='ew')
 
-            value_label = ttk.Label(self, text=value, justify='center')
+            value_label = ttk.Label(self, text=value, justify='center',font=('Arial', 20))
             value_label.grid(row=i + 1, column=1, padx=40, pady=10, sticky='ew')
         pass
 
