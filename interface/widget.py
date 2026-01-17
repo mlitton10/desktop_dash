4a2608bf75e2abd7c98cc0373c043a2d172a2811
mlitton10 <mattlitton10@gmail.com>
2026-01-16 18:24:04 -0800

    Create generic panel class, weather panel that inherits

--- interface/widget.py
+++ interface/widget.py
@@ -2,6 +2,28 @@ import tkinter as tk
 import ttkbootstrap as ttk
 
 
+
+class Panel(ttk.Frame):
+    def __init__(self, master, panel_name):
+        super().__init__(master)
+
+        self.title_text = ttk.Label(self, text=panel_name, font=('Arial', 80))
+        self.title_text.grid(row=0, column=1, padx=0, pady=0, sticky='nsew')
+
+class WeatherPanel(Panel):
+    def __init__(self, master, weather_dict, panel_name='Weather Forecast'):
+        super().__init__(master, panel_name)
+
+        self.display_1 = WeatherDisplayFrame(self, weather_dict)
+        self.display_1.grid(row=1, column=0, padx=10, pady=(10, 0), sticky="nsew")
+
+        self.display_2 = WeatherDisplayFrame(self, weather_dict)
+        self.display_2.grid(row=1, column=1, padx=10, pady=(10, 0), sticky="nsew")
+
+        self.display_3 = WeatherDisplayFrame(self, weather_dict)
+        self.display_3.grid(row=1, column=2, padx=10, pady=(10, 0), sticky="nsew")
+
+
 class WeatherDisplayFrame(ttk.Frame):
     def __init__(self, master, weather_dict):
         # This is a template frame for a weather display. Fake data for now
